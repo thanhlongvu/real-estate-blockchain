6e6f4deee9bdeab26d3b116050318859592f63ba
longvt <learncode360@gmail.com>
2018-12-01 09:46:33 +0700

    Install react-box done!

---
+++ truffle.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+
+  },
+  // to customize your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "client/src/contracts")
+};
